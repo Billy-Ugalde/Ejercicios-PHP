4e1f5bcf694d4441240dd77c2460dca2548d67c0
Billy-Ugalde <billyugalde2004@gmail.com>
2026-07-07 18:39:32 -0600

    Solicitud de inserción

---
+++ Practicas/ejercicios/resources/views/insert.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Post insertado correctamente</h1>
+@endsection
--- Practicas/ejercicios/routes/web.php
+++ Practicas/ejercicios/routes/web.php
@@ -1,29 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\DB;
+//use App\Http\Controllers\UserController;
 
-Route::inertia('/', 'welcome')->name('home');
+// Route::inertia('/', 'welcome')->name('home');
 
-//ejercicios de rutas
+// //ejercicios de rutas
 
-Route::get('/bye', function () {
-    return 'Hasta luego';
-})-> name("Chao");
+// Route::get('/bye', function () {
+//     return 'Hasta luego';
+// })-> name("Chao");
 
-Route::get('/hello', function () {
-    return 'Hola, ¿como estas?';
+// Route::get('/hello', function () {
+//     return 'Hola, ¿como estas?';
+// });
+
+// Route::get('/edad/{id}/{nombre}', function ($id, $nombre) {
+//     return "Tengo " . $id . " años y me llamo " . $nombre;
+// });
+
+// Route::get('admin/posts/example', function () {
+//     $url = route("admin.home"); //se usa el alias para no escribir toda la ruta
+//     return "Esta es la url actual: " . $url;
+// })->name('admin.home');
+// //registro de ruta controlador
+// Route::resource('user', UserController::class);
+// Route::get('contact', [UserController::class, 'contact']);
+// Route::get('post/{id}/{name}/{password}', [UserController::class, 'show_post']);
+
+Route::get('/insert', function () {
+
+    DB::insert('insert into posts(title, content, name) values(?, ?,?)', ['PHP con Laravel', 'Laravel', 'James']);
+
+    return view('insert');
 });
-
-Route::get('/edad/{id}/{nombre}', function ($id, $nombre) {
-    return "Tengo " . $id . " años y me llamo " . $nombre;
-});
-
-Route::get('admin/posts/example', function () {
-    $url = route("admin.home"); //se usa el alias para no escribir toda la ruta
-    return "Esta es la url actual: " . $url;
-})->name('admin.home');
-//registro de ruta controlador
-Route::resource('user', UserController::class);
-Route::get('contact', [UserController::class, 'contact']);
-Route::get('post/{id}/{name}/{password}', [UserController::class, 'show_post']);
