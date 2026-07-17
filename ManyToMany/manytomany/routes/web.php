c616cf2cf36deba629d9fef26dfa1629a87134fd
Billy-Ugalde <billyugalde2004@gmail.com>
2026-07-17 16:17:56 -0600

    Uso de relación muchos a muchos: modelo Role y asignación de roles a usuarios

--- ManyToMany/manytomany/routes/web.php
+++ ManyToMany/manytomany/routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::get('/create', function () {
+    $user = User::find(1);
+
+    $user->roles()->save(new Role(['name' => 'Administrator']));
+});
