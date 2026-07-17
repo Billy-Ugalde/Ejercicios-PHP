9499e1b41c1b55fe55f44cac3ca424cd4977e2d7
Billy-Ugalde <billyugalde2004@gmail.com>
2026-07-17 14:44:37 -0600

    Uso de relación uno a muchos con posts

--- OneToMany/onetomany/routes/web.php
+++ OneToMany/onetomany/routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::get('/create', function () {
+    $user = User::findOrFail(1);
+
+    $user->posts()->save(new Post(['title' => 'Mi primera publicación de Billy', 'body' => 'Amo Laravel, con Fabian']));
+});
