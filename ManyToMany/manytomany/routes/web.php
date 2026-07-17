36c77659f70cd40598482581b6cb6c22b36eeff6
Billy-Ugalde <billyugalde2004@gmail.com>
2026-07-17 17:40:05 -0600

    polymorphic

--- ManyToMany/manytomany/routes/web.php
+++ ManyToMany/manytomany/routes/web.php
@@ -11,3 +11,51 @@ Route::get('/create', function () {
 
     $user->roles()->save(new Role(['name' => 'Administrator']));
 });
+
+Route::get('/read', function () {
+    $user = User::findOrFail(1);
+
+    foreach ($user->roles as $role) {
+        echo $role->name;
+    }
+});
+
+Route::get('/update', function () {
+    $user = User::findOrFail(1);
+
+    if ($user->has('roles')) {
+        foreach ($user->roles as $role) {
+            if ($role->name == 'Administrator') {
+                $role->name = 'subscriber';
+
+                $role->save();
+            }
+        }
+    }
+});
+
+Route::get('/delete', function () {
+    $user = User::findOrFail(1);
+
+    foreach ($user->roles as $role) {
+        $role->whereId(1)->delete();
+    }
+});
+
+Route::get('/attach', function () {
+    $user = User::findOrFail(1);
+
+    $user->roles()->attach(6);
+});
+
+Route::get('/detach', function () {
+    $user = User::findOrFail(1);
+
+    $user->roles()->detach();
+});
+
+Route::get('/sync', function () {
+    $user = User::findOrFail(1);
+
+    $user->roles()->sync([6, 7]);
+});
