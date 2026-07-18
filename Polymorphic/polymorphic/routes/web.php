f47793f8f9e3573f9c35c67cd9dfb1f7274329e1
Billy-Ugalde <billyugalde2004@gmail.com>
2026-07-17 19:44:26 -0600

    Creating a New Project with some Migrations - Classwork

--- Polymorphic/polymorphic/routes/web.php
+++ Polymorphic/polymorphic/routes/web.php
@@ -1,5 +1,36 @@
 <?php
 
+use App\Models\Staff;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::get('/create', function () {
+    $staff = Staff::find(1);
+
+    $staff->photos()->create(['path' => 'example.jpg']);
+});
+
+Route::get('/read', function () {
+    $staff = Staff::findOrFail(1);
+
+    foreach ($staff->photos as $photo) {
+        return $photo->path;
+    }
+});
+
+Route::get('/update', function () {
+    $staff = Staff::findOrFail(1);
+
+    $photo = $staff->photos()->whereId(1)->first();
+
+    $photo->path = "Updated example.jpg";
+
+    $photo->save();
+});
+
+Route::get('/delete', function () {
+    $staff = Staff::findOrFail(1);
+
+    $staff->photos()->whereId(1)->delete();
+});
