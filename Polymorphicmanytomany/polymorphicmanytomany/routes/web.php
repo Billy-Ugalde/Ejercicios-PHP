9d550ebeeae6b4bd65cc3e4d51a6378681b78c9d
Billy-Ugalde <billyugalde2004@gmail.com>
2026-07-21 10:14:52 -0600

    Rutas de prueba create/delete en morphManyToMany

--- Polymorphicmanytomany/polymorphicmanytomany/routes/web.php
+++ Polymorphicmanytomany/polymorphicmanytomany/routes/web.php
@@ -1,5 +1,26 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Video;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::get('/create', function () {
+    $post = Post::create(['name' => 'My first post']);
+    $tag1 = Tag::find(1);
+    $post->tags()->save($tag1);
+
+    $video = Video::create(['name' => 'video.mov']);
+    $tag2 = Tag::find(2);
+    $video->tags()->save($tag2);
+});
+
+Route::get('/delete', function () {
+    $post = Post::find(1);
+
+    foreach ($post->tags as $tag) {
+        $tag->whereId(1)->delete();
+    }
+});
