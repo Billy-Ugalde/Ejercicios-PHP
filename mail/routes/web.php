91c24cec0817e693e72ccd8a3cd05ee1186f11d3
Billy-Ugalde <billyugalde2004@gmail.com>
2026-07-26 19:40:07 -0600

    Instalación de dependencias de mailgun y rutas

--- mail/routes/web.php
+++ mail/routes/web.php
@@ -1,11 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+Route::get('/test-mail', function () {
+    $data = [
+        'title' => 'Hola, esto es una prueba de correos',
+        'content' => 'En  el curso de laravel',
+    ];
+
+    Mail::send('emails.test', $data, function ($message) {
+        $message->to('fb7270321@gmail.com', 'Fabian')->subject('Hola, ¿cómo estás?');
+    });
+
+    return 'Email sent';
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
